268007a2f73f86fe7620b7676daf868117a34e39
Karol Wach <karolzpoznania@wp.pl>
2013-04-04 20:10:03 +0200

    Added dynamic subjects selection and frequency stats prototype

--- routes/frequency.js
+++ routes/frequency.js
@@ -1,15 +1,20 @@
 var Analyzer = require('../lib/analyzer');
 
-function render(req, res, start, end) {
+function render(req, res, start, end, removeSubjects) {
     req.schedule(function(schedule) {
         req.attendance(function(att) {
-            if(!!start) {
+            if (!!start) {
                 start = new Date(start);
                 start.setHours(0);
             }
-            if(!!end) {
+            if (!!end) {
                 end = new Date(end);
                 end.setHours(0);
+            }
+            if (removeSubjects) {
+                var subjects = schedule.subjects;
+                for (var i = 0; i < removeSubjects.length; subjects.remove(removeSubjects[i++]));
+                schedule = schedule.reduce(subjects);
             }
             var analyzer = new Analyzer(schedule, att.period(start, end));
             var days = [], frequency = [], message;
@@ -22,6 +27,7 @@ function render(req, res, start, end) {
             res.render((req.get('X-Requested-With') ? 'includes/frequency' : 'frequency'), {
                 days : analyzer.days,
                 frequency : analyzer.frequency,
+                subjects : schedule.subjects,
                 message : message
             });
         });
@@ -33,5 +39,5 @@ exports.index = function(req, res) {
 };
 
 exports.indexPost = function(req, res) {
-    render(req, res, req.body.start, req.body.end);
+    render(req, res, req.body.start, req.body.end, req.body.removeSubjects);
 };
