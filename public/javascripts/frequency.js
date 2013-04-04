b3d41c6ffa38280f389f4c44fcfb4d63a7f6b9be
Karol Wach <karolzpoznania@wp.pl>
2013-04-04 20:10:03 +0200

    Added dynamic subjects selection and frequency stats prototype

--- public/javascripts/frequency.js
+++ public/javascripts/frequency.js
@@ -5,7 +5,7 @@ Frequency.init = function() {
         $this = $(this);
         var next = $this.next();
         if (next.is(':visible')) {
-            next.fadeOut(function() {
+            next.fadeOut('fast', function() {
                 $this.css('background-image', 'url(\'../images/toggle_plus.png\')');
             });
         } else {
@@ -15,16 +15,34 @@ Frequency.init = function() {
         }
     });
     $('#start, #end').change(Frequency.load);
+    var timer;
+    $('.subjects-bar, .subjects').hover(function() {
+        clearTimeout(timer);
+        $('.subjects').fadeIn();
+    }, function() {
+        timer = setTimeout(function() {
+            $('.subjects').clearQueue().fadeOut('fast');
+        }, 200);
+    });
+    $('.subjects #all').change(function() {
+        $('.subjects :checkbox').prop('checked', $(this).is(':checked'));
+    });
+    $('.subjects :checkbox').change(Frequency.load);
 };
 
 Frequency.load = function() {
     $.post('#', {
-        start: $('#start').val(),
-        end: $('#end').val()
+        start : $('#start').val(),
+        end : $('#end').val(),
+        removeSubjects : $('.subjects :checkbox:not(#all)').map(function(i, el) {
+            $el = $(el);
+            return !$el.is(':checked') && $el.val() || undefined;
+        }).toArray()
     }, ajaxLoader(function(data) {
         $data = $(data);
         $('.days').html($data.filter('.days').html());
         $('.frequency').html($data.filter('.frequency').html());
+        $('.stats').html($data.filter('.stats').html());
     }));
 };
 
